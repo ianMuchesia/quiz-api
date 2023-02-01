55ce29984cf447ae598195f20636e64aad0e862f
ianMuchesia <ianmuchesia@yahoo.com>
2023-02-01 16:12:56 +0300

    bug fixes

--- backend/controllers/getQuiz.js
+++ backend/controllers/getQuiz.js
@@ -1,22 +1,17 @@
 import axios from 'axios'
-
+import Post from "../models/model.js"
 
 const getQuiz = async(req, res)=>{
-   const {category, difficulty} = req.query
-   let fetchUrl = "https://the-trivia-api.com/api/questions?limit=5"
-   if(category){
-        fetchUrl += `&categories=${category}`
-       // res.send(fetchUrl)
-   }
-   if(difficulty){
-       fetchUrl += `&difficulty=${difficulty}`  
-   }
-   if(region){
-    fetchUrl += `&region=${region}`
-   }
+   
+  try {
+    const response = await Post.find({})
+    res.status(200).json({msg:response})
+  } catch (error) {
+    console.log(error)
+  }
+   
  
-   const {data} = await axios.get(fetchUrl)
-   res.status(200).json({data}) 
+  
 }
 
 export default getQuiz
--- backend/controllers/postQuiz.js
+++ backend/controllers/postQuiz.js
@@ -1,6 +1,33 @@
 import axios from "axios"
+import Post from "../models/model.js"
+
+
 
 const postQuiz=async(req, res)=>{
-    res.status(200).json({msg: req.body})
+   
+    try {
+        const {category, region, difficulty} = req.body
+        let fetchUrl = "https://the-trivia-api.com/api/questions?limit=5"
+        if(category){
+            fetchUrl += `&categories=${category}`
+           // res.send(fetchUrl)
+       }
+       if(difficulty){
+           fetchUrl += `&difficulty=${difficulty}`  
+       }
+       if(region){
+        fetchUrl += `&region=${region}`
+       }
+       const {data} = await axios.get(fetchUrl)
+       
+         const newPost = await Post.create({
+            question:data
+        })
+        console.log("yess")
+        res.status(201).json({success: true, data: newPost}) 
+      
+    } catch (error) {
+        res.status(500).json({success: false , message:error})
+    }
 }
 export default postQuiz;
